cd3523afd9535f467e5df1539ce69c4fe8dcd273
cheshire <gpffhdnvlcl@naver.com>
2024-03-19 20:43:46 +0900

    init: Service 구조 초기 셋팅
    Former-commit-id: 49fe0fafca93a52e7b951d5a87f0c43841d43150

---
+++ backend/src/main/java/com/mayak/chuckchuck/service/PillService.java
@@ -0,0 +1,11 @@
+package com.mayak.chuckchuck.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PillService {
+}
---
+++ backend/src/main/java/com/mayak/chuckchuck/service/TakeListService.java
@@ -0,0 +1,4 @@
+package com.mayak.chuckchuck.service;
+
+public class TakeListService {
+}
