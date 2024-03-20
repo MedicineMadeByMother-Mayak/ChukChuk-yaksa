79d667b4367dc9887d6f7092fa312566ef0dadad
jinak_Local <ssafytou2@gmail.com>
2024-03-20 14:22:33 +0900

    feat : 약효 효과 삭제
    Former-commit-id: c12bbb773ae875b69bd198c877d05712c0b833d0

---
+++ backend/src/main/java/com/mayak/chuckchuck/repository/UserPillEffectRepository.java
@@ -0,0 +1,7 @@
+package com.mayak.chuckchuck.repository;
+
+import com.mayak.chuckchuck.domain.UserPillEffect;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPillEffectRepository extends JpaRepository<UserPillEffect, Long> {
+}
