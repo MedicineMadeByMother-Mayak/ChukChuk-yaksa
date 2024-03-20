29a15530448fbc2fb5e4f6819a56c49a1f97e757
jinak_Local <ssafytou2@gmail.com>
2024-03-20 14:35:45 +0900

    feat : 약효 효과 삭제 (주석 추가), 추후에 예외처리 필요
    Former-commit-id: d6985181f31172c32266f79a8216e5ce72d1051e

--- backend/src/main/java/com/mayak/chuckchuck/service/UserPillEffectService.java
+++ backend/src/main/java/com/mayak/chuckchuck/service/UserPillEffectService.java
@@ -12,6 +12,12 @@ import org.springframework.stereotype.Service;
 public class UserPillEffectService {
     private final UserPillEffectRepository userPillEffectRepository;
 
+    /**
+     * 약효기록 삭제
+     * @author 최진학
+     * @param userPillEffectId (약효 기록 id)
+     * @return 없음
+     */
     public void updateUserPillEffectIsDelete(Long userPillEffectId) {
         UserPillEffect userPillEffect = userPillEffectRepository.findById(userPillEffectId).get();
         userPillEffect.getCommonData().toggleDelete();
