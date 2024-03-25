fe6400d3547d8b1dff7b4ff6b686ce953642f249
jinak_Local <ssafytou2@gmail.com>
2024-03-25 09:49:38 +0900

    Docs: pill 테이블 deafult 값 업데이트 & 변경
    Former-commit-id: 0c42b8a06d4e935ec5de637e3a2b760b20bc8f6e

--- setting/DB/4. update (24.03.20).sql
+++ setting/DB/4. update (24.03.20).sql
@@ -1,3 +1,4 @@
+-- 24.03.20 업데이트 내역
 -- SQL_SAFE_UPDATES 설정 변경
 SET SQL_SAFE_UPDATES = 0;
 
@@ -10,3 +11,24 @@ SET SQL_SAFE_UPDATES = 0;
 ALTER TABLE `chukchuk_db`.`user` 
 CHANGE COLUMN `height` `height` int NULL DEFAULT NULL,
 CHANGE COLUMN `weight` `weight` int NULL DEFAULT NULL;
+
+
+
+-- 24.03.25 업데이트 내역
+-- 약 테이블 default 변경
+ALTER TABLE `pill`
+MODIFY COLUMN `warning_pregnant` BOOLEAN DEFAULT false,
+MODIFY COLUMN `warning_use_date` BOOLEAN DEFAULT false,
+MODIFY COLUMN `warning_elders` BOOLEAN DEFAULT false,
+MODIFY COLUMN `warning_together` BOOLEAN DEFAULT false;
+
+
+
+-- 약 테이블 default false, 전부 true로 변경 (테스트 편하게 하기 위해서)
+update pill
+	set warning_pregnant = true,
+		warning_use_date = true,
+		warning_elders = true, 
+		warning_together = true,
+        image_url = "../setting/DB/MySQL Safe Update 해제.png",
+        type = "진통제 (painkiller)";
