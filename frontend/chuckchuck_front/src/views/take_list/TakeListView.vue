b2578a2e62a8b30af7a5dba0cdf7d205f2598028
bo kyung kim <rapsparrow99@naver.com>
2024-03-27 17:55:53 +0900

    feat: CSS 추가

--- frontend/chuckchuck_front/src/views/take_list/TakeListView.vue
+++ frontend/chuckchuck_front/src/views/take_list/TakeListView.vue
@@ -1,6 +1,6 @@
 <template>
   <Wave title="복용 관리" height="60px" />
-  <vueper-slides class="no-shadow ex--center-mode" :arrows-outside="false" bullets-outside transition-speed="250" :interval="2000">
+  <vueper-slides autoplay class="no-shadow ex--center-mode" :arrows-outside="false" bullets-outside transition-speed="250" :interval="2000">
   <vueper-slide
     class = 'vueper-slide'
     v-for="i in 3"
@@ -18,11 +18,16 @@
       <div>복용중</div>
     </div>
     <div class="menu-right">
-      <button class="gray">과거에먹은약</button>
-      <button class="blue">추가</button>
+      <button class="gray-button">과거에먹은약</button>
+      <button class="navy-button">추가</button>
     </div>
   </div>
   <hr class="line">
+  <div class="pill-info" v-for="(data, index) in dumydata.result" :key="index">
+    <div>{{data.createDate}} [{{ data.takeListName }}]</div>
+    <img src="@/assests/icon/edit.png" alt="홈">
+
+  </div>
   
  
 </template>
@@ -34,22 +39,42 @@
   // import { ref, onUnmounted } from 'vue';
 
   export default {
-  components: { VueperSlides, VueperSlide, Wave }
-}
+  components: { VueperSlides, VueperSlide, Wave},
+
+  setup() {
+    const dumydata = {
+      count: 1, // 건수
+      result: [
+        {
+          createDate: "2024-03-04",
+          takeListName: "위염약",
+          pills: [
+            { type: "기침 및 감기 치료제", name: "페니라민정" },
+            { type: "항히스타민제", name: "프라닥사캡슐" }
+          ]
+        }
+      ]
+    };
+    return {dumydata};
+  }
+};
+
+  
 </script>
   
 <style>
+
 .ex--center-mode {
-  margin-top: 30px;
+  margin-top: 0px;
   left: 50%;
   transform: translateX(-50%); 
-  width: 80%;
+  width: 100%;
 }
 .vueper-slide {
   position: fixed;
   left: 50%;
   transform: translateX(-50%); 
-  width: 70%;
+  width: 90%;
   height: 80px;
   display: flex;
   align-items: center;
@@ -59,6 +84,19 @@
   z-index: 1000;
   transition: top 0.3s ease;
 }
+.vueperslides__bullet .default {
+  background-color: rgba(0, 0, 0, 0.3);
+  border: none;
+  box-shadow: none;
+  transition: 0.3s;
+  width: 10px;
+  height: 10px;
+  margin-left: -10px;
+  margin-top: 0px;
+  
+}
+.vueperslides__bullet--active .default {background-color: rgb(66, 66, 66);}
+
 
 .alarms {
    background-color: lightskyblue;
@@ -72,8 +110,8 @@
   margin-left: 20px;
   margin-top: 10px;
   position: relative;
+  overflow: hidden;
 }
-
 .menu-left {
   display: flex;
   align-items: center;
@@ -82,7 +120,6 @@
   width: 80px;
   margin-right: 5px;
 }
-
 .menu-right {
   text-align: right;
   margin-bottom: 10px;
@@ -97,7 +134,7 @@ button {
   margin-right: 10px;
   margin-top: 10px;
 }
-.gray {
+.gray-button {
     background-color: #cccccc;
     color: #ffffff; 
     font-weight: bold; 
@@ -108,7 +145,7 @@ button {
     cursor: pointer; 
     margin-right: 5px;
   }
-  .blue {
+  .navy-button {
     background-color: #001f3f;
     color: #ffffff; 
     font-weight: bold; 
@@ -120,10 +157,21 @@ button {
   }
 
 .line{
-  color: gray;
   width: 90%;
-  margin-top: 20px;
-  
+  margin-top: 0px; 
 }
+.pill-info {
+  margin-left: 20px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+}
+.pill-info p{
+  flex: 1;
+}
+.pill-info img {
+  width: 20px; 
+  height: 20px;
 
+}
 </style>
