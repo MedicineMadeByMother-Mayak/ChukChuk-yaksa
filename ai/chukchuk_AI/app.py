9e2ccdf3c4a6d6b7946406f3a3a174883482c3c0
HuiJung <tom1997tom@gmail.com>
2024-03-29 17:23:43 +0900

    #70 - fix: wsgi 서버 사용하기위한 코드 수정
    wsgi 서버 사용하기위한 코드 수정
    
    
    Former-commit-id: 1e3987745d2552cb90403ec2a8e815a80c30f046

--- ai/chukchuk_AI/app.py
+++ ai/chukchuk_AI/app.py
@@ -1,5 +1,5 @@
 import os
-
+from yolov5 import detect_custom
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -10,12 +10,10 @@ def upload_image():
     if 'image' not in request.files:
         return 'No file part'
 
-    ROOT = r'C:\Users\SSAFY\Desktop\SSAFY-2\sec_pjt\S10P22B304\ai\chukchuk_AI\imags'
-
     file = request.files['image']
 
     # 이미지를 저장할 디렉토리 경로
-    input_folder = os.path.join(ROOT, r'\input')
+    input_folder = os.path.join('yolov5', 'runs', 'input')
 
     # 디렉토리가 존재하지 않으면 생성
     if not os.path.exists(input_folder):
@@ -25,8 +23,13 @@ def upload_image():
     file_path = os.path.join(input_folder, file.filename)
     file.save(file_path)
 
-    return 'Image uploaded successfully'
+    result = detect_custom.run(source=file_path)
 
+    response = jsonify({
+        'result': result
+    })
+
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
