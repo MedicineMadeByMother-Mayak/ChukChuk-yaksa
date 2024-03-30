18115cfbd15fb1ccecfd5e4b5975bfa2f16549f5
rlaxodhks770 <rlaxodhks770@naver.com>
2024-03-30 22:53:52 +0900

    중간 커밋

--- frontend/chuckchuck_front/src/views/user_pill_effect/UserPillEffectView.vue
+++ frontend/chuckchuck_front/src/views/user_pill_effect/UserPillEffectView.vue
@@ -11,13 +11,21 @@
       ></SearchBar>
     </div>
     <div class="buttons">
-      <button class="tab-button">전체</button
+      <div>
+        <button class="tab-button">전체</button
       ><button class="tab-button">효과</button
       ><button class="tab-button">중단</button
       ><button class="tab-button">부작용</button>
+      </div>
+      <div>
+        <div>
+        <button class="navy-button"><font-awesome-icon icon="fa-duotone fa-circle-plus" style="--fa-primary-color: #242291; --fa-secondary-color: #ffffff;" />기록추가</button>
+      </div>
+      </div>
     </div>
   </HeaderForm>
   <PillContent></PillContent>
+  <font-awesome-icon icon="fa-duotone fa-circle-plus" style="--fa-primary-color: #242291; --fa-secondary-color: #ffffff;" />
 </template>
 
 <script setup>
@@ -35,22 +43,40 @@ import SearchBar from "@/common/SearchBar.vue";
 
 .tab-button {
   font-family: Cafe24SsurroundAir;
+  font-size: 11px;
   border: none;
   background-color: white;
-  width: 60px;
+  width: 55px;
   padding: 3px 10px;
   margin: 0px;
   cursor: pointer;
-  clip-path: polygon(20% 0, 80% 0, 100% 100%, 0% 100%);
+  clip-path: polygon(10% 0, 90% 0, 100% 100%, 0% 100%);
   caret-color: transparent;
+  border-top-left-radius: 5%;
+  position: relative;
+  overflow: hidden;
 }
 
 .buttons {
+  width: 320px;
   display: flex;
   justify-content: space-between;
   margin: 0px 0px;
   position: absolute;
   left: 0;
   bottom: 0;
+}
+
+.navy-button {
+  background-color: #242291;
+  color: #ffffff;
+  font-weight: bold;
+  width: 65px;
+  height: 21px;
+  border-radius: 5px;
+  padding: 3px px;
+  font-size: 11px;
+  border: none;
+  cursor: pointer;
 }
 </style>
