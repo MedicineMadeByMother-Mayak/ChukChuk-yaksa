7edf047dd94250d08f6c71beba62f08e9bfd98bd
rlaxodhks770 <rlaxodhks770@naver.com>
2024-04-02 14:12:33 +0900

    중간 마지

--- frontend/chuckchuck_front/src/common/Form/AddModalForm.vue
+++ frontend/chuckchuck_front/src/common/Form/AddModalForm.vue
@@ -1,8 +1,8 @@
 <!-- 사용법 RouterLink편
 <template>
     <ModalForm v-model="msg" :modalData="[
-    ['원하는 텍스트', RouterLink면 true axios면 false, {params:{ 전송하는 데이터 }, Link:'연결을 원하는 링크'}],
-    ['원하는 텍스트', RouterLink면 true axios면 false, {params:{ 전송하는 데이터 }, Link:'연결을 원하는 링크'}],
+    ['strong에 원하는 텍스트', '그냥 원하는 텍스트', RouterLink면 true axios면 false, {params:{ 전송하는 데이터 }, Link:'연결을 원하는 링크'],
+    ['strong에 원하는 텍스트', '그냥 원하는 텍스트', RouterLink면 true axios면 false, {params:{ 전송하는 데이터 }, Link:'연결을 원하는 링크'],
     ]" /> 
 </template> 
 
@@ -13,8 +13,8 @@
 -->
 
 <template>
-  <div class="modal-overlay" v-if="showModal" @click="closeModal">
-    <div class="modal" style="padding: 10px">
+  <div class="modal-overlay" v-if="msg" @click="closeModal">
+    <div class="modal">
       <ul class="modal-menu">
         <li v-for="(item, index) in modalData" :key="index">
           <RouterLink
@@ -22,12 +22,19 @@
             :to="{ name: item[3].Link, params: item[3].params }"
             class="router-link-item"
           >
-            <div style="width: 100%; height: 100%; font-size: 14px">
+            <div style="margin: 9px; font-size: 14px">
               <strong>{{ item[0] }}</strong
               >{{ item[1] }}
             </div>
           </RouterLink>
-          <hr v-if="index < modalData.length - 1" />
+
+          <div v-else class="router-link-item" @click="$emit(item[3].emitName)">
+            <div style="margin: 9px; font-size: 14px">
+              <strong>{{ item[0] }}</strong
+              >{{ item[1] }}
+            </div>
+          </div>
+          <hr style="margin: 0px 10px" v-if="index < modalData.length - 1" />
         </li>
       </ul>
     </div>
@@ -35,18 +42,23 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const showModal = defineModel();
+const msg = defineModel();
+defineEmits(["addPillInTakeList"]);
 const props = defineProps({
   modalData: {
     type: Array,
-    default: [["원하는 텍스트 넣으세요", true, { params: {}, Link: "home" }]],
+    default: [
+      [
+        "원하는 텍스트 넣으세요",
+        true,
+        { params: {}, Link: "home", emitName: "메서드 이름" },
+      ],
+    ],
   },
 });
 
 const closeModal = () => {
-  showModal.value = !showModal.value;
+  msg.value = !msg.value;
 };
 </script>
 
@@ -103,6 +115,5 @@ const closeModal = () => {
 .router-link-item {
   text-decoration: none;
   color: inherit;
-  padding: 1px 1px;
 }
 </style>
