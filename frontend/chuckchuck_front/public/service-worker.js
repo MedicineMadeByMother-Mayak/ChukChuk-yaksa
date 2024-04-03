12a2de3acd042b853073861e592d8a1a71e373ac
차현철 <ckguscjf03@gmail.com>
2024-04-04 05:29:29 +0900

    #103 - fix: 최신버전 유지하도록 PWA 수정

--- frontend/chuckchuck_front/public/service-worker.js
+++ frontend/chuckchuck_front/public/service-worker.js
@@ -1,37 +1,65 @@
-// service-worker.js
+const CACHE_NAME = "app-static-v2"; // 버전을 변경하여 업데이트를 관리
+const assetsToCache = [
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-256x256.png",
+  "/icons/icon-384x384.png",
+  // ... 캐시하고 싶은 다른 자산들
+];
 
-self.addEventListener("install", function (event) {
+// 설치 이벤트
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Install Event processing");
+
   event.waitUntil(
-    caches.open("app-static-v1").then(function (cache) {
-      return cache.addAll([
-        "/manifest.webmanifest",
-        "/icons/icon-192x192.png",
-        "/icons/icon-256x256.png",
-        "/icons/icon-384x384.png",
-        // ... 캐시하고 싶은 다른 자산들
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[Service Worker] Caching all: app shell and content");
+      return cache.addAll(assetsToCache);
     })
   );
 });
 
-// 여기에 fetch 이벤트 리스너 등 다른 서비스 워커 로직을 추가할 수 있습니다.
-self.addEventListener("fetch", function (e) {
+// 활성화 이벤트: 오래된 캐시를 정리
+self.addEventListener("activate", (event) => {
+  console.log("[Service Worker] Activate event");
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              console.log("[Service Worker] Deleting old cache:", cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        console.log(
+          "[Service Worker] Claiming clients for version",
+          CACHE_NAME
+        );
+        return self.clients.claim();
+      })
+  );
+});
+
+// 패치 이벤트
+self.addEventListener("fetch", (event) => {
   // 'chrome-extension' 스키마와 'GET' 메소드가 아닌 요청은 무시
-  if (!e.request.url.startsWith("http") || e.request.method !== "GET") {
-    return fetch(e.request);
+  if (!event.request.url.startsWith("http") || event.request.method !== "GET") {
+    return fetch(event.request);
   }
 
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      console.log("[Service Worker] Fetching resource: " + e.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
       return (
-        r ||
-        fetch(e.request).then(function (response) {
-          return caches.open("app-static-v1").then(function (cache) {
-            console.log(
-              "[Service Worker] Caching new resource: " + e.request.url
-            );
-            cache.put(e.request, response.clone());
+        response ||
+        fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
             return response;
           });
         })
